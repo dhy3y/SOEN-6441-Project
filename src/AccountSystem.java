77f90de6dda9509ed76aaef9dfc7be162bcf1da0
Dhyey Doshi <dhyeydoshi2512@gmail.com>
2023-03-11 17:20:40 -0500

    Update AccountSystem.java

--- src/AccountSystem.java
+++ src/AccountSystem.java
@@ -2,17 +2,28 @@ import java.util.HashMap;
 
 public class AccountSystem {
 
-    public static HashMap<String, Boolean> rentDetails;
+    private HashMap<String, Boolean> rentDetails;
 
-    public static void addLease(String leaseID, boolean rentPaidOrNot) {
+    private static AccountSystem instance = new AccountSystem();
+
+    private AccountSystem() {}
+
+    public static AccountSystem getInstance() {
+        if(instance == null)
+            return new AccountSystem();
+        
+        return instance;
+    }
+
+    public void addLease(String leaseID, boolean rentPaidOrNot) {
         rentDetails.put(leaseID, rentPaidOrNot);
     }
 
-    public static void changeRentStatus(String leaseID, boolean newStatus) {
+    public void changeRentStatus(String leaseID, boolean newStatus) {
         rentDetails.put(leaseID, newStatus);
     }
 
-    public static void getRentStatus(String leaseID) {
+    public void getRentStatus(String leaseID) {
         if(rentDetails.get(leaseID)) {
             System.out.println("Paid");
         }
