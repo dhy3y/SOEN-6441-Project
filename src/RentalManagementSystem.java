b5d3603c640f1e25c3807a5786c2c8b7fcea05db
ArjunAnghan <arjunanghan2020@gmail.com>
2023-03-11 17:03:46 -0500

    arjun push

---
+++ src/RentalManagementSystem.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class RentalManagementSystem {
+    public static HashMap<String, HashMap<String, Object>> propertyDetails = new HashMap<String, HashMap<String, Object>>();
+    public static HashMap<String, Object> tenantDetails = new HashMap<String, Object>();
+    public static HashMap<String, Object> leaseDetails = new HashMap<String, Object>();
+
+}
+class Driver
+{
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
+
