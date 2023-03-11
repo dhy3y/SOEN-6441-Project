605f96855a7e1c9c4be233cad7b89660787edaff
Dhyey Doshi <dhyeydoshi2512@gmail.com>
2023-03-11 16:42:22 -0500

    Update AccountSystem.java

--- src/AccountSystem.java
+++ src/AccountSystem.java
@@ -4,7 +4,15 @@ public class AccountSystem {
 
     HashMap<String, Boolean> rentDetails;
 
-    public void rentStatus(String leaseID) {
+    public void addLease(String leaseID, boolean rentPaidOrNot) {
+        rentDetails.put(leaseID, rentPaidOrNot);
+    }
+
+    public void changeRentStatus(String leaseID, boolean newStatus) {
+        rentDetails.put(leaseID, newStatus);
+    }
+
+    public void getRentStatus(String leaseID) {
         if(rentDetails.get(leaseID)) {
             System.out.println("Paid");
         }
